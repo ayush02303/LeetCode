c719fc2529af4b6497787236ebf054f69d76cd64
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-20 00:23:26 +0600

    Time: 0 ms (100%), Space: 24.6 MB (13.55%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -4,7 +4,7 @@ public:
         int count=0; 
         for(int i=0 ;i<nums.size();i++)
         {
-            for(int j=0;j<nums.size();j++)
+            for(int j=i+1;j<nums.size();j++)
             {
                 if(nums[i]+nums[j] < target && i<j)
                 {
