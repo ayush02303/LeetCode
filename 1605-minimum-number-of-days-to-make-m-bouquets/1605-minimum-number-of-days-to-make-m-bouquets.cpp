6fec2b76a9c5ee9d7e5d5e138881390ab784b659
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-21 00:18:46 +0600

    Time: 36 ms (11.25%), Space: 123.3 MB (9.37%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
-bool possible(vector<int> &arr, int day, int m, int k) {
-    int n = arr.size(); //size of the array
-    int cnt = 0;
-    int noOfB = 0;
-    // count the number of bouquets:
-    for (int i = 0; i < n; i++) {
-        if (arr[i] <= day) {
-            cnt++;
-        }
-        else {
-            noOfB += (cnt / k);
-            cnt = 0;
-        }
-    }
-    noOfB += (cnt / k);//why
-    return noOfB >= m;
-}
-    int minDays(vector<int>& arr, int m, int k) {
-          long long val = m * 1ll * k * 1ll;
-    int n = arr.size(); //size of the array
-    if (val > n) return -1; //impossible case.
-    //find maximum and minimum:
-    /*int mini = INT_MAX, maxi = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        mini = min(mini, arr[i]);
-        maxi = max(maxi, arr[i]);
-    }*/
+    int possible(vector<int> nums ,int mid, int m, int k)
+    {
+        int n= nums.size(); 
+        int count=0; 
+        int bquet=0; 
 
-    //apply binary search:
-    int low = *min_element(arr.begin(),arr.end()), high = *max_element(arr.begin(),arr.end());
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (possible(arr, mid, m, k)) {
-            high = mid - 1;
+        for(int i=0; i<n;i++)
+        {
+            if(nums[i] <= mid)
+            {
+                count+=1; 
+            }
+            else
+            {
+                 bquet += (count / k);
+                 count = 0;
+            }
+            
         }
-        else low = mid + 1;
-    }
-    return low;
+         bquet += (count / k);//why
+    return bquet >= m;
+    };
+    int minDays(vector<int>& nums, int m, int k) {
+        int low = *min_element(nums.begin(),nums.end()); 
+        int high= *max_element(nums.begin(),nums.end()); 
+        //int val = m*k;
+         //long long val = m * 1ll * k * 1ll; 
+         long long val= (long long)(m) *(long long)(k); 
+         int n= nums.size(); 
+         if(val > n) return -1; 
+         while(low<= high)
+         {
+            int mid = low+(high-low)/2; 
+
+            if(possible(nums,mid,m,k))
+            {
+                high=mid-1; 
+            }
+            else
+            {
+                low=mid+1; 
+            }
+         }
+        return low; 
+
         
     }
 };
