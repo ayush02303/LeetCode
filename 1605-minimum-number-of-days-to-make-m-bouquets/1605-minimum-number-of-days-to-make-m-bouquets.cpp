8b371285be72ae11d7ce867c8d5534f41d405587
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-21 12:29:36 +0600

    Time: 22 ms (63.02%), Space: 70.7 MB (15.2%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    int possible(vector<int> nums ,int mid, int m, int k)
+    int calculate(vector<int>&nums, int mid, int m, int k)
     {
-        int n= nums.size(); 
         int count=0; 
-        int bquet=0; 
-
-        for(int i=0; i<n;i++)
+        int bouquets=0; 
+        for(int i=0; i<nums.size();i++)
         {
             if(nums[i] <= mid)
             {
@@ -14,36 +12,36 @@ public:
             }
             else
             {
-                 bquet += (count / k);
-                 count = 0;
+                bouquets += (count/k); 
+                count=0; 
             }
-            
         }
-         bquet += (count / k);//why
-    return bquet >= m;
+         bouquets += (count / k);//why
+    return bouquets >= m;
     };
     int minDays(vector<int>& nums, int m, int k) {
-        int low = *min_element(nums.begin(),nums.end()); 
-        int high= *max_element(nums.begin(),nums.end()); 
-        //int val = m*k;
-         //long long val = m * 1ll * k * 1ll; 
-         long long val= (long long)(m) *(long long)(k); 
-         int n= nums.size(); 
-         if(val > n) return -1; 
-         while(low<= high)
-         {
-            int mid = low+(high-low)/2; 
+        int low= *min_element(nums.begin(),nums.end()); 
+        int high=*max_element(nums.begin(),nums.end()); 
+        int n = nums.size(); 
+        long long  val = (long long)(m) * (long long)(k);
+        if(n<val) return -1; 
+        // if(val > n) return -1;
 
-            if(possible(nums,mid,m,k))
+        while(low<=high)
+        {
+            int mid= low+(high-low)/2; 
+
+            if( calculate(nums,mid,m,k))
             {
-                high=mid-1; 
+                high= mid-1; 
             }
             else
             {
                 low=mid+1; 
             }
-         }
-        return low; 
+        }
+        return low;
+
 
         
     }
