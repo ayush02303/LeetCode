46d87521c33fca170f92cf4b9e3267a7dfd1b98e
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-21 21:51:12 +0600

    Time: 12 ms (54.92%), Space: 35.1 MB (34.81%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    int  calculate(vector<int>& nums, int mid)
+    int calculate(vector<int> &nums, int mid)
     {
         int days=1; 
         int load=0; 
-
-        for(int i=0; i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(load+nums[i] > mid)
+            if( load+nums[i] > mid)
             {
                 days+=1; 
-                load= nums[i];  
-
+                load=nums[i]; 
             }
             else
             {
-                load= load+ nums[i]; 
+                load=load+ nums[i]; 
             }
         }
-        return days;
-    }; 
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low= *max_element(weights.begin(),weights.end()); 
-        int high = accumulate(weights.begin(),weights.end(),0); 
+        return days; 
+    };
+
+    int shipWithinDays(vector<int>& nums, int days) {
+        int low= *max_element(nums.begin(),nums.end()); 
+        int high= accumulate(nums.begin(),nums.end(),0); //returns total sum of array
 
         while(low<=high)
         {
-            int mid = low+(high-low)/2; 
-            int t = calculate(weights,mid); 
+            int mid= low+(high-low)/2; 
 
+            int t= calculate(nums,mid); 
             if(t <= days)
             {
-                high = mid-1; 
+                high=mid-1;
             }
             else
             {
                 low=mid+1; 
             }
+
+
         }
         return low; 
     }
