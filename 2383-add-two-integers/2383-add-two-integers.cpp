01f61c4cdb9fbb6d80c0ced97f1884cf71346625
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-22 10:53:05 +0600

    Time: 0 ms (100%), Space: 8.1 MB (2.27%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.cpp
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        int c= num1+num2; 
-        return c;
+        int temp; 
+        temp= num1+num2; 
+        return temp; 
         
     }
 };
