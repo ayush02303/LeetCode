b50c5ca8f926bb629c7cf911c57ae3251fc0585e
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-24 11:08:46 +0600

    Time: 0 ms (100%), Space: 28.4 MB (25.78%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] % 2 == 0)
-                nums[i] = 0;
-            else
-                nums[i] = 1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2==0){
+                nums[i]=0; 
+            }
+            else{
+                nums[i]=1; 
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        sort(nums.begin(),nums.end()); 
+        return nums; 
+        
     }
 };
