c165c334afa758e925b66da15121f13c8103744d
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-24 21:08:53 +0600

    Time: 0 ms (100%), Space: 17.7 MB (17.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,59 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1;
-        int last = -1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                first = i;
-                break;
+ int lowerBound(vector<int>& arr, int target) {
+        // code here
+        int n =arr.size(); 
+        int low=0; 
+        int high=n-1;
+        int ans=n;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2; 
+            
+            if(arr[mid] >= target)
+            {
+                ans= mid;
+                high=mid-1; 
+                
+            }
+            else
+            {
+                low=mid+1; 
             }
         }
+        return ans; 
+    }
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] == target) {
-                last = i;
-                break;
+     int upperBound(vector<int>& arr, int target) {
+        // code here
+        int n =arr.size(); 
+        int low=0; 
+        int high=n-1;
+        int ans=n;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2; 
+            
+            if(arr[mid] > target)
+            {
+                ans= mid;
+                high=mid-1; 
+                
+            }
+            else
+            {
+                low=mid+1; 
             }
         }
-
-        return {first, last};
+        return ans; 
+    }
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int n=arr.size();
+        int lb= lowerBound(arr, target);
+        if(lb==n || arr[lb] != target) return {-1,-1};
+        return{lb,upperBound(arr, target)-1};
+        
     }
 };
