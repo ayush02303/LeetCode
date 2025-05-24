32b0aefdf225ee5b46a6243958629748c964679b
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-24 22:49:00 +0600

    Time: 0 ms (100%), Space: 17.6 MB (17.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,63 +1,22 @@
 class Solution {
 public:
-    int binary1(vector<int>&nums ,int target){
-        int low= 0; 
-        int high = nums.size()-1; 
-        int first =-1; 
-
-        while(low<= high){
-            int mid = low+(high-low)/2; 
-
-            if(nums[mid] == target){
-                first =mid; 
-                high=mid-1; 
-            }
-            else if(nums[mid] < target){
-                 low=mid+1; 
-            }
-            else
-            {
-                high=mid-1; 
-            }
-               
-            
-        }
-        return first;
-
-    };
-
-     int binary2(vector<int>&nums ,int target){
-        int low= 0; 
-        int high = nums.size()-1; 
-        int last =-1; 
-
-        while(low<= high){
-            int mid = low+(high-low)/2; 
-
-            if(nums[mid] == target){
-                last=mid; 
-                low= mid+1 ; 
-            }
-            else if(nums[mid] < target){
-                low=mid+1; 
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return last;
-
-    };
-
-
-     
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = binary1(nums,target); 
-        int last = binary2(nums,target); 
+        int first = -1, last=-1; 
 
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == target){
+                first=i; 
+                break; 
+            }
+        }
+
+         for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i] == target){
+                last =i; 
+                break; 
+            }
+        }
         return {first,last}; 
-        
         
     }
 };
