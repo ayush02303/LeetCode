cc58c4b0534a83c0dd0eb592d2e2b3ac9d90e6ac
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-24 22:37:13 +0600

    Time: 0 ms (100%), Space: 17.7 MB (17.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,59 +1,63 @@
 class Solution {
 public:
- int lowerBound(vector<int>& arr, int target) {
-        // code here
-        int n =arr.size(); 
-        int low=0; 
-        int high=n-1;
-        int ans=n;
-        
-        while(low<=high)
-        {
-            int mid = (low+high)/2; 
-            
-            if(arr[mid] >= target)
-            {
-                ans= mid;
-                high=mid-1; 
-                
-            }
-            else
-            {
-                low=mid+1; 
-            }
-        }
-        return ans; 
-    }
+    int binary1(vector<int>&nums ,int target){
+        int low= 0; 
+        int high = nums.size()-1; 
+        int first =-1; 
 
-     int upperBound(vector<int>& arr, int target) {
-        // code here
-        int n =arr.size(); 
-        int low=0; 
-        int high=n-1;
-        int ans=n;
-        
-        while(low<=high)
-        {
-            int mid = (low+high)/2; 
-            
-            if(arr[mid] > target)
-            {
-                ans= mid;
+        while(low<= high){
+            int mid = low+(high-low)/2; 
+
+            if(nums[mid] == target){
+                first =mid; 
                 high=mid-1; 
-                
+            }
+            else if(nums[mid] < target){
+                 low=mid+1; 
             }
             else
             {
+                high=mid-1; 
+            }
+               
+            
+        }
+        return first;
+
+    };
+
+     int binary2(vector<int>&nums ,int target){
+        int low= 0; 
+        int high = nums.size()-1; 
+        int last =-1; 
+
+        while(low<= high){
+            int mid = low+(high-low)/2; 
+
+            if(nums[mid] == target){
+                last=mid; 
+                low= mid+1 ; 
+            }
+            else if(nums[mid] < target){
                 low=mid+1; 
             }
+            else{
+                high=mid-1;
+            }
         }
-        return ans; 
-    }
-    vector<int> searchRange(vector<int>& arr, int target) {
-        int n=arr.size();
-        int lb= lowerBound(arr, target);
-        if(lb==n || arr[lb] != target) return {-1,-1};
-        return{lb,upperBound(arr, target)-1};
+        return last;
+
+    };
+
+
+     
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = binary1(nums,target); 
+        int last = binary2(nums,target); 
+
+        return {first,last}; 
+        
         
     }
 };
