9beb4697f32860198d62279f5a4a7ea1fa118c2e
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-26 08:35:50 +0600

    Time: 4 ms (23.89%), Space: 26.5 MB (15.68%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_set<int> mp;
-        vector<int> ans;
+        sort(nums.begin(),nums.end()); 
+        unordered_set<int>st; 
+        vector<int>ans;
 
-        for (auto num : nums) {
-            if (mp.count(num)) {
-                ans.push_back(num);
+        for( auto num : nums){
+            if(st.count(num)){
+                ans.push_back(num); 
             }
-            mp.insert(num);
+            st.insert(num); 
+
         }
         return ans;
+        
     }
 };
