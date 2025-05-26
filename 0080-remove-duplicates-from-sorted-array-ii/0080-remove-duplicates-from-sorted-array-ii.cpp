08f41eaacecdf6aba88709c65431f18e1f561e4d
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-26 20:13:32 +0600

    Time: 7 ms (73.68%), Space: 21.6 MB (6.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int>mp; 
+        unordered_map<int,int>st; 
         vector<int>ans; 
 
-        for( auto num : nums){
-            if( mp[num] < 2){
-                ans.push_back(num); 
-            } 
-            mp[num]++; 
+        for( auto num: nums){
+            if(st[num] < 2){
+                ans.push_back(num);
+            }
+            st[num]++; 
         }
         nums= ans; 
         return ans.size(); 
+        
     }
 };
