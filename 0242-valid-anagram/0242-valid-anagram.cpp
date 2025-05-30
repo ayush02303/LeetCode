c105488c29aadd78cb8e7b00fd0f1e9f980b7982
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-05-30 10:28:53 +0600

    Time: 5 ms (30.89%), Space: 9.8 MB (48.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,7 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end()); 
         sort(t.begin(),t.end()); 
-        return s==t; 
+        if(s == t) return true; 
+        else return false;
         
     }
 };
