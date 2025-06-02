051d49b08351660d7e68b5d127d912c7939ec6df
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-02 07:20:37 +0530

    Time: 0 ms (100%), Space: 9.1 MB (6.27%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -3,10 +3,10 @@ public:
     bool checkIfPangram(string sentence) {
         unordered_map<char,int>mp; 
 
-        for( auto c : sentence){
+        for( auto c: sentence){
             mp[c]++; 
         }
-        return mp.size()==26; 
+        return mp.size() == 26 ;
         
     }
 };
