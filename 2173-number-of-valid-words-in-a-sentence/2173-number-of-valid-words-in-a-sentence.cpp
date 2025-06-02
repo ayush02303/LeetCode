917c61eed1af9f7152b61afabab5e1cc554a1521
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-02 10:04:14 +0530

    Time: 6 ms (19.57%), Space: 10.3 MB (65.84%) - LeetHub

--- 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.cpp
+++ 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    bool isValid(string& word) {
-        int n = word.size();
-        int hyphenCount = 0, punctuationCount = 0;
+    bool isValid( string &word){
+        int n = word.size() ;
+        int hyphencount =0, punctuationcount=0; 
 
-        for (int i = 0; i < n; ++i) {
-            char c = word[i];
+        for( int i=0 ; i< word.size() ;i++){
+            char c = word[i]; 
 
-            if (isdigit(c)) return false;
+            if(isdigit(c)) return false ;
 
-            if (c == '-') {
-                hyphenCount++;
-                if (hyphenCount > 1) return false;
-                if (i == 0 || i == n - 1) return false;
-                if (!islower(word[i - 1]) || !islower(word[i + 1])) return false;
+            if( c == '-'){
+                hyphencount+=1; 
+                if(hyphencount > 1) return false; 
+                if( i ==0 || i== n-1) return false ;
+                if( !islower(word[i-1]) || !islower(word[i+1])) return false;
             }
 
-            if (c == '!' || c == '.' || c == ',') {
-                punctuationCount++;
-                if (punctuationCount > 1 || i != n - 1) return false;
+            if( c== '!' || c == '.' || c == ','){
+                punctuationcount++; 
+                if( punctuationcount > 1 || i != n-1) return false ;
             }
 
-            if (!islower(c) && c != '-' && c != '!' && c != '.' && c != ',') {
-                return false;
+            if( !islower(c) && c != '-' && c!= '!' && c != '.' && c != ','){
+                return false; 
             }
         }
+        return true ;
 
-        return true;
-    }
-
+    };
     int countValidWords(string sentence) {
-        istringstream iss(sentence);
-        string word;
-        int count = 0;
+        istringstream iss( sentence);
+            string word; 
+            int count=0; 
 
-        while (iss >> word) {
-            if (isValid(word)) count++;
+            while( iss >> word){
+                if( isValid(word)) count+=1; 
+            }
+             return count; 
+
         }
-
-        return count;
-    }
-};
+       
+        
+    };
