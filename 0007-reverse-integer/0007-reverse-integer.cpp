db7770a279faf0d250cc9c50e4a3797250ffa4e4
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-03 10:57:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (97.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        long rev = 0;
+        long rev =0; 
 
-        while (x != 0) {
-            int digit = x % 10;
-            rev = rev * 10 + digit;
-            x /= 10;
+        while( x!=0){
+           // x = x- '0';
+            int rem = x%10; 
+            rev = rev*10 + rem ; 
+
+            x/=10;
         }
-
-        // Check for 32-bit signed integer overflow
-        if (rev > INT_MAX || rev < INT_MIN) {
-            return 0;
-        }
-
-        return rev;
+        if( rev > INT_MAX ||rev < INT_MIN) return 0;
+        return rev; 
     }
 };
