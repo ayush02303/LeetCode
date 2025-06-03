6996e0f6df7f6c9e46a45a3853f3294e0c2674e2
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-03 15:13:09 +0530

    Time: 2352 ms (5.29%), Space: 10.9 MB (94.63%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
-        int count=0; 
+        int n = s.size(); 
+        int count =0; 
 
-        for (int i = 0; i < n; i++) {
-            bool a= false, b = false , c= false; 
-            for (int j = i; j < n; j++) {
-                if (s[j] == 'a')
-                    a =true;
-                if (s[j] == 'b')
-                    b= true;
-                if (s[j] == 'c')
-                    c = true; 
+        for(int i=0 ; i< n ; i++){
+            bool a = false,b= false, c= false;
+            for(int j=i ; j< n ;j++){
+                if( s[j] == 'a') a= true; 
+                if( s[j] == 'b') b = true; 
+                if( s[j] == 'c') c = true;
 
-                    if( a && b && c){
-                        count += (n-j); 
-                        break;
-                    }
+                if(a && b && c) {
+                    count+=(n-j); 
+                    break; 
+                } 
+
             }
         }
-        return  count;
+        return count; 
     }
 };
