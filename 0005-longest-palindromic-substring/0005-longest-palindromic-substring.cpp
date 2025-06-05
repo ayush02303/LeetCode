be1a5d231506c35962f1d5652cc9b3fc76792f23
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-05 09:42:37 +0530

    Time: 851 ms (11.21%), Space: 9.3 MB (93.08%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    // Helper function to check if a string is a palindrome
-    bool isPalindrome(const string& str, int left, int right) {
-        while (left < right) {
-            if (str[left] != str[right])
-                return false;
-            left++;
-            right--;
+    bool isPalindrome( const string &s ,int i , int j){
+        //int low =0 ; 
+       // int high = j ; 
+        while( i < j){
+            if(s[i] != s[j]) return false ; 
+            i++; 
+            j--; 
         }
         return true;
+
+
     }
-
     string longestPalindrome(string s) {
-        int n = s.length();
-        int maxLen = 0;
-        int start = 0;
+        int maxlength =0 ;
+        int start =0; // starting index 
+        int n = s.length(); 
+        for( int i=0 ; i< n ; i++){
+            for( int j = i; j<n ;j++){
+                if(  isPalindrome(s,i,j) ){
+                    int length = j-i+1; 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j)) {
-                    int len = j - i + 1;
-                    if (len > maxLen) {
-                        maxLen = len;
-                        start = i;
+                    if(length > maxlength){
+                        maxlength = length; 
+                        start = i; 
                     }
                 }
             }
         }
-
-        return s.substr(start, maxLen);
+        return s.substr(start,maxlength);
     }
 };
