68651403a84bf6811819b6fe5bf4d9ba2d3503f6
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-06 21:13:05 +0530

    Time: 1 ms (15.33%), Space: 8.9 MB (32.88%) - LeetHub

--- 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool areNumbersAscending(string s) {
-        stringstream ss(s);
-        string word; 
-        int curr=0,prev=0; 
+        stringstream ss(s); 
+        string word;
+        string curr; 
+        string prev; 
 
-        while( ss>> word){
-            if(isdigit(word[0])){
+        while(ss >> word){
+            if( isdigit(word[0])){
                 curr = stoi(word); 
-                if(curr<= prev) return false; 
-                prev = curr; 
+
+                if( curr <= prev) return false;
+                 prev = curr; 
             }
            
-        }
-         return true;
+        } 
+        return true; 
     }
 };
