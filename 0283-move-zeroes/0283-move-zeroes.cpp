2a64b2454e1c5423d988a1aaa488a24f05e4333e
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-08 07:17:34 +0530

    Time: 0 ms (100%), Space: 24.3 MB (8.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         vector<int>ans(nums.size(),0); 
-        int i=0;
-        for(auto num : nums){
-            if(num !=0){
-                ans[i++]= num;
-            }
+        int i =0; 
+        for( auto num : nums){
+            if(num !=0) ans[i++] = num; 
         }
         nums = ans; 
-       
         
         
     }
