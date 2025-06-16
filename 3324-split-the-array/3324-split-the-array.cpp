f4b730875a3ef3d4742033584faf1a8c03db57a6
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-16 06:25:26 +0530

    Time: 0 ms (100%), Space: 25.2 MB (57.97%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.cpp
+++ 3324-split-the-array/3324-split-the-array.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-            unordered_map<int,int>mp; 
-
+        unordered_map<int,int>mp; 
         for( auto num : nums){
             if(mp[num] == 2) return false; 
-            mp[num]++; 
+            mp[num]++;
         }
         return true;
-        
     }
 };
