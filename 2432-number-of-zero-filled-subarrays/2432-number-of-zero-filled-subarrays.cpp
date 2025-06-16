22b458c8ec16e7d8232a7d7657f0cb51d329ea0e
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-16 10:39:00 +0530

    Time: 4 ms (27.8%), Space: 111.5 MB (11.81%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -6,10 +6,11 @@ public:
         for (int num : nums) {
             if (num == 0) {
                 count++;
-                result += count;
+                
             } else {
                 count = 0;
             }
+            result += count;
         }
 
         return result;
