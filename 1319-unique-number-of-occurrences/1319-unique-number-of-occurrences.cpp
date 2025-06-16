d407d345aa49e33498a698bec06a3441556b176a
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-16 14:37:46 +0530

    Time: 0 ms (100%), Space: 12 MB (29.95%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> mp;
-
-        // Step 1: Count frequencies
-        for (int num : arr) {
-            mp[num]++;
+        unordered_map<int,int>mp; 
+        for( auto num : arr){
+            mp[num]++; 
         }
-
-        unordered_set<int> seenFrequencies;
-
-        // Step 2: Check for uniqueness of frequencies
-        for (auto pair : mp) {
-            if (seenFrequencies.count(pair.second)) {
-                return false; // Duplicate frequency found
-            }
-            seenFrequencies.insert(pair.second);
+        unordered_set<int>st; 
+        for( auto num : mp){
+            if( st.count(num.second)) return false; 
+            st.insert(num.second); 
         }
-
-        return true; // All frequencies are unique
+        return true;
     }
 };
