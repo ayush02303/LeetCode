a031700138689d893ca7c00483980c2be7f85ea1
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-24 19:17:54 +0530

    Time: 4 ms (35.3%), Space: 79.3 MB (37.67%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,36 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> s1, s2;
+        stack<int>s1,s2;
 
-        // Push all digits of l1 and l2 onto stacks
-        while (l1) {
-            s1.push(l1->val);
-            l1 = l1->next;
-        }
-        while (l2) {
-            s2.push(l2->val);
+        while(l1!= NULL){
+            s1.push(l1->val); 
+            l1 = l1->next; 
+
+        } 
+        while(l2!=NULL){
+            s2.push(l2->val); 
             l2 = l2->next;
+
         }
-
         int carry = 0;
-        ListNode* result = nullptr;
+        ListNode* result = NULL; 
 
-        // Perform addition
-        while (!s1.empty() || !s2.empty() || carry > 0) {
-            int a = (!s1.empty()) ? s1.top() : 0;
-            int b = (!s2.empty()) ? s2.top() : 0;
+        while(!s1.empty() || !s2.empty() || carry >0){
+            int a = (!s1.empty()) ? s1.top() : 0; 
+            int b = (!s2.empty()) ? s2.top() : 0; 
 
-            if (!s1.empty()) s1.pop();
-            if (!s2.empty()) s2.pop();
+            if(!s1.empty()) s1.pop(); 
+            if(!s2.empty()) s2.pop(); 
 
-            int sum = a + b + carry;
-            carry = sum / 10;
+            int sum = a+b+carry; 
 
-            // Insert new node at the front
-            ListNode* newNode = new ListNode(sum % 10);
-            newNode->next = result;
-            result = newNode;
+            carry = sum/10; 
+
+            ListNode* newnode = new ListNode(sum%10); 
+            newnode->next = result; 
+            result = newnode;  
+
         }
 
         return result;
