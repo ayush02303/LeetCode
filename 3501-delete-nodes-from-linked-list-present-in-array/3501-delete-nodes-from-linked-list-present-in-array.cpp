53efec6ab5b12b78403f2d61571c02f6c4f3645d
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-27 21:16:17 +0530

    Time: 143 ms (26.43%), Space: 277.1 MB (21.56%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,33 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> st; 
+        unordered_set<int>st; 
         ListNode* temp = head; 
-
-        // Step 1: Store all values from the list
-        while (temp != NULL) {
-            st.insert(temp->val); 
+        while(temp!= NULL){
+            st.insert(temp->val);
             temp = temp->next; 
         }
-
-        // Step 2: Erase values that are in nums
-        for (auto num : nums) {
+        for( auto num : nums){
             st.erase(num); 
         }
-
-        // Step 3: Rebuild the list with only values remaining in the set
-        ListNode* dummy = new ListNode(-1); // dummy node for easier construction
-        ListNode* curr = dummy; 
         temp = head; 
+        ListNode* dummy = new ListNode(-1); 
+        ListNode* curr = dummy; 
 
-        while (temp != NULL) {
-            if (st.count(temp->val)) {
+        while(temp!= NULL){
+            if( st.count(temp->val)){
                 curr->next = new ListNode(temp->val); 
                 curr = curr->next;
             }
+
             temp = temp->next; 
         }
-
-        return dummy->next; // new head of the modified list
+        return dummy->next; 
     }
 };
