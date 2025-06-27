acab284a1d9485978c3dedb8765e9031ecc58d1c
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-27 19:53:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.cpp
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int n = people.size();
-        
-        
-        //sort the array
-        sort(begin(people), end(people));
-        
-        int i = 0, j = n-1;
-        
-        int boats = 0;
-        
-        while(i <= j) {
-            if(people[j] + people[i] <= limit) {
-                i++;
-                j--;
-            } else {
-                j--;
+        int n = people.size(); 
+        int low = 0; 
+        int high = n-1; 
+        int boats = 0; 
+        sort(people.begin(),people.end()); 
+        while( low<= high){
+            if( people[i] + people[j] <= limit){
+                i++; j--; 
+                boats+=1; 
             }
-            boats += 1;
+            else{
+                j--; 
+                boats+=1; 
+            }
         }
-        
         return boats;
     }
 };
