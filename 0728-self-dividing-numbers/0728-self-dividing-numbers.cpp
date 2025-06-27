faa23f2db0e442a9c3eacec5531de91bd52ec1ae
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-27 11:20:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (27.29%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool isSelfDividing(int num) {
-        int n = num;
-        while (n != 0) {
-            int digit = n % 10;
-            if (digit == 0 || num % digit != 0) return false;
-            n /= 10;
+    bool isValid( int n){
+        int temp = n; 
+        while( temp!= 0){
+            int rem = temp%10; 
+            if( rem == 0 || n%rem != 0) return false;
+
+            temp = temp/10;
         }
         return true;
-    }
-
+    };
     vector<int> selfDividingNumbers(int left, int right) {
-        vector<int> result;
-        for (int i = left; i <= right; ++i) {
-            if (isSelfDividing(i)) {
-                result.push_back(i);
+        vector<int>ans; 
+        for( int i = left ; i<= right ; i++){
+            if( isValid(i)){
+                ans.push_back(i); 
             }
         }
-        return result; 
+        return ans;
     }
 };
