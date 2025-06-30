0e8b1e70c33f7d3be2f32cd7a436c8c9abb3ba8b
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-06-30 19:12:48 +0530

    Time: 0 ms (100%), Space: 12.3 MB (96.18%) - LeetHub

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-        // approach 2, using min and max 
-        //tc on2
         int mini = INT_MAX, maxi = INT_MIN; 
         for( auto num : nums){
-             mini = min(mini,num); 
-             maxi = max(maxi,num); 
+            mini = min(mini,num); 
+            maxi = max(maxi,num); 
         }
         int count = 0; 
         for( auto num : nums){
-            if( num > mini && num <  maxi) count+=1; 
+            if( num> mini  && num < maxi) count+=1; 
         }
         return count;
+        
     }
 };
