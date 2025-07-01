ee5e380a3d03a3cae0c63256faf08d45408b29dd
ayush02303 <142813464+ayush02303@users.noreply.github.com>
2025-07-01 21:32:38 +0530

    Time: 2 ms (92.76%), Space: 15.3 MB (8.6%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,39 +17,21 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if( head == NULL) return NULL; 
-        unordered_map<Node*,Node*>mp;
-
         Node* curr = head; 
-        Node* prev = NULL;
-        Node* newHead = NULL;
+        unordered_map<Node*,Node*>mp; 
 
-        while(curr!= NULL){
-            Node* temp = new Node(curr->val); 
-            mp[curr] = temp; 
-
-            if(newHead == NULL){
-                newHead = temp; 
-                prev = newHead;
-            }
-            else{
-                prev->next = temp; 
-                prev = temp; 
-            }
+        while(curr!= NULL) // while(curr) would also suffice
+        {
+            mp[curr] = new Node(curr->val); 
             curr = curr->next; 
-        } 
-        curr = head; 
-        Node* newCurr = newHead; 
-
-        while(curr){
-            if(curr->random == NULL) newCurr->random = NULL; 
-            else newCurr->random = mp[curr->random];
-
-            curr = curr->next; 
-            newCurr = newCurr->next;
-
         }
-        return newHead; 
-        
+        curr = head; 
+        while(curr!= NULL){
+            mp[curr]->next = mp[curr->next]; 
+            mp[curr]->random = mp[curr->random];
+            
+            curr = curr->next; 
+        }
+        return mp[head];
     }
 };
