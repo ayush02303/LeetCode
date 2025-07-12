6d939f700991eba791c637710c08d844808f970a
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-12 09:42:28 +0530

    Time: 0 ms (100%), Space: 14.4 MB (11.67%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -5,7 +5,7 @@ public:
         int count = 0; 
         
         for( auto num : nums){
-            if( (to_string(num).length())%2 ==0  ) count+=1;
+            if( ( to_string(num).length() ) %2 ==0  ) count+=1;
         }
         return count;
     }
