952a4918a94b21989e99bb03b2b13cda28599254
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-12 08:26:28 +0530

    Time: 0 ms (100%), Space: 13.7 MB (74.16%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    int compress(vector<char>& chars) {
-        int n = chars.size();
-        
-        int i     = 0;
+    int compress(vector<char>& s) {
         int index = 0;
+        int i = 0; 
+        int n = s.size();
         
-        while(i < n) {
-            char curr = chars[i];
-            
-            int count = 0;
-            //Find count of duplicates
-            while(i < n && chars[i] == curr) {
+
+        while( i<n){
+            char curr = s[i]; 
+            int count = 0; 
+
+            while( i< n && s[i] == curr){
+                count+=1; 
                 i++;
-                count++;
             }
-            
-            //Assign it to chars and move index ahead to add the count
-            chars[index] = curr;
+            s[index] = curr; 
             index++;
-            
-            //add the count
-            if(count > 1) {
-                string count_str = to_string(count);
-                for(char &ch : count_str) {
-                    chars[index] = ch;
+
+            if( count>1){
+                string count_str = to_string(count); 
+
+                for( char c : count_str){
+                    s[index] = c; 
                     index++;
                 }
             }
         }
-        
         return index;
     }
 };
