7fddae7291c729d3671abf18f4967b09fb7ce7c3
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-15 12:52:19 +0530

    Time: 6 ms (40.03%), Space: 15.2 MB (15.9%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
@@ -1,23 +1,18 @@
-//S.C : O(1)
 class Solution {
 public:
-    int MOD = 1e9+7;
-    
+     int MOD = 1e9+7;
     int countHomogenous(string s) {
-        int n = s.length();
-        int result = 0;
-        int count  = 0;
-        
-        for(int i = 0; i<n; i++) {
-            if(i > 0 && s[i] == s[i-1]) {
-                count++;
-            } else {
-                count = 1;
-            }
-            
-            result = (result + count) % MOD;
+
+        int n = s.size(); 
+        int length  = 0; 
+        int result = 0; 
+
+        for( int i = 0 ; i< n ; i++){
+            if( i> 0 && s[i] == s[i-1]) length+=1; 
+            else length = 1; 
+
+            result =  (result + length)% MOD; 
         }
-        
         return result;
     }
 };
