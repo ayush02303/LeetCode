4ad4cbfda156647e2249fdf24f0cafcc6d3624fb
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-15 14:48:27 +0530

    Time: 4 ms (18.72%), Space: 11.6 MB (43.85%) - LeetHub

---
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    
+    int numSub(string s) {
+        int n = s.size(); 
+        int length = 0; 
+        long long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                length++;
+                result = (result + length) % MOD;
+            } else {
+                length = 0;  // reset on '0'
+            }
+        }
+
+        return result;
+    }
+};
