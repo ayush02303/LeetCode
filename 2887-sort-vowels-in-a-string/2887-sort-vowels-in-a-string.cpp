99dca0599666b6c2ed507b61b6650eddee1e88d4
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-18 08:24:29 +0530

    Time: 15 ms (58.1%), Space: 15.5 MB (93.88%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     bool isVowel( char ch){
         ch = tolower(ch); 
-        return ( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-    };
+        return ( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' );
+    }
     string sortVowels(string s) {
 
-        string temp ; 
+        int n = s.size(); 
+        string temp; 
 
-        for( int i = 0 ; i< s.size() ; i++){
-            if( isVowel(s[i])) temp.push_back(s[i]); 
+        for( char c : s){
+            if(isVowel(c)) temp.push_back(c); 
         }
-        sort( temp.begin(), temp.end()); 
-        int j= 0; 
 
-        for( int i = 0 ; i< s.size() ; i++){
-            if( isVowel(s[i])) s[i] = temp[j++]; 
+        sort(temp.begin(), temp.end());
+        int j = 0;
+        for( int i = 0 ; i< n ; i++){
+            if( isVowel( s[i])) s[i] = temp[j++];
         }
+
         return s;
     }
 };
