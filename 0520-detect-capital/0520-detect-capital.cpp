20e7ec92cebc4a0741f9ea11112271725420fcc6
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-19 11:12:42 +0530

    Time: 3 ms (4.23%), Space: 8.1 MB (50.36%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int upperCount = 0;
-        for (char c : word) {
-            if (isupper(c)) upperCount++;
+        int uppercount = 0; 
+        int n = word.size(); 
+
+        for( char ch : word){
+            if( isupper(ch)) uppercount+=1; 
         }
 
-        int n = word.length();
-
-        // Case 1: All letters uppercase
-        if (upperCount == n) return true;
-
-        // Case 2: All letters lowercase
-        if (upperCount == 0) return true;
-
-        // Case 3: Only the first letter is uppercase
-        if (upperCount == 1 && isupper(word[0])) return true;
-
-        return false;
+        if( uppercount == n ) return true; 
+        else if( uppercount == 0) return true; 
+        else if ( uppercount == 1 && isupper(word[0])) return true; 
+        else return false;        
     }
 };
