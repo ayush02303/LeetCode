9b761847ed5073d8b7760aafa986de09ad6c975d
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-20 09:07:28 +0530

    Time: 11 ms (45.25%), Space: 19.4 MB (51.73%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
+        
         int n = s.size(); 
-
         int arr1[26] = {0}; 
-        int arr2[26] = {0}; //26 as there are 26 letters in english 
+        int arr2[26] = {0}; 
 
         for( int i = 0 ; i< n ; i++){
             arr1[s[i] - 'a']++; 
             arr2[t[i] - 'a']++; 
         }
-        int result = 0; 
+
+        int result = 0 ; 
 
         for( int i = 0 ; i< 26 ; i++){
-            if( arr1[i] > arr2[i]) result+= arr1[i] - arr2[i]; 
-
+           if( arr1[i] > arr2[i]) result += arr1[i] - arr2[i];
         }
-        return result ; 
-        
+        return result; 
     }
 };
