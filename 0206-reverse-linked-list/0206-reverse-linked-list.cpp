58e372220f36ee60d5b5eef901718786260e8e59
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-23 12:13:25 +0530

    Time: 0 ms (100%), Space: 13.7 MB (10.5%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,13 +11,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        stack<int>st; 
+          stack<int>st; 
         ListNode* temp = head; 
-
         while( temp!= NULL){
-            st.push(temp->val); 
-            temp = temp->next; 
+            st.push(temp->val);
+            temp =temp->next; 
         }
+        
         temp = head; 
         while( temp!= NULL){
             temp->val = st.top(); 
@@ -25,5 +25,6 @@ public:
             temp = temp->next; 
         }
         return head;
+        
     }
 };
