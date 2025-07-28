645da72849f8007edbcdca131b633ead7ce2f92e
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-28 09:17:11 +0530

    Time: 0 ms (100%), Space: 31.3 MB (79.79%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,13 +4,15 @@ public:
         int low = 0; 
         int high = nums.size()-1; 
 
-        while( low<= high){
-            int mid = low + ( high - low)/2; 
-            if( nums[mid ]== target )return mid; 
-            else if( nums[mid] < target ) low = mid +1 ; 
-            else high = mid - 1; 
+
+        while( low<= high ){
+            int mid = low + (high - low)/2; 
+            if( nums[mid] == target) return mid; 
+            else if( nums[mid] < target) low = mid + 1; 
+            else high = mid-1; 
+
         }
-        return -1; 
+        return -1;
         
     }
 };
