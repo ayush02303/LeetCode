8c7b0efdad20fad63f4cfbdd552ff0793048d045
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-29 22:13:35 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.93%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-
-        int n = s1.size(); 
-
         int diffs = 0; 
+        int n = s1.size(); 
 
         int freq1[26] = {0}; 
         int freq2[26] = {0}; 
 
-        for( int i = 0; i< n ; i++){
-            if( s1[i] != s2[i] ) diffs+=1; 
-            if( diffs > 2) return false; 
+        for( int i = 0 ; i< n ; i++){
+            if( s1[i] != s2[i]) diffs+=1; 
+
+            if( diffs>2) return false; 
 
             freq1[s1[i] - 'a']++; 
-            freq2[s2[i] - 'a']++; 
-
+            freq2[s2[i] -'a']++; 
         }
 
         for( int i = 0 ; i < 26 ; i++){
-            if( freq1[i] != freq2[i]) return false;  
+            if( freq1[i] != freq2[i]) return false;
         }
-        return true; 
+
+        return true;
 
         
     }
