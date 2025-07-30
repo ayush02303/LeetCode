00ec8f2f76fde67393b0a13f0957b3672754fb5e
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-30 09:17:00 +0530

    Time: 0 ms (100%), Space: 9 MB (68.25%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.cpp
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        int count  =0; 
-        string result; 
-        for( char ch : s){
-            if( ch ==  ' ') count+=1; 
-            if(count == k) break; 
-            result += ch; 
+        int count = 0; 
+        string result;
+
+        for( auto ch : s){
+            if( ch == ' ') count+=1; 
+            if( count == k) break; 
+            result+=ch;
         }
         return result;
     }
