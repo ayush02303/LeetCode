498a805cc8ebea6623b5148d9c7effd91aa26548
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-07-30 09:36:50 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,8 +3,12 @@ public:
     void reverseString(vector<char>& s) {
         int low = 0; 
         int high = s.size()-1; 
+
         while( low<= high){
-            swap(s[low] , s[high]); 
+            char temp = s[low]; 
+            s[low] = s[high]; 
+            s[high] = temp; 
+
             low++; 
             high--;
         }
