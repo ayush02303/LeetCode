621588448301128c712c32420a04f7f1867051d2
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-01 08:21:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        sort( nums.begin() , nums.end()); 
+        sort( nums.begin() , nums.end());
         
     }
 };
