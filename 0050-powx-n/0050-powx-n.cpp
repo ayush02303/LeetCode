6b655208e7d6382110bcf2aaeb58b58e44059fca
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-02 09:38:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = pow(x,n); 
+        long long m = n;        // use long long to prevent overflow
+        if (m < 0) {
+            x = 1 / x;
+            m = -m;
+        }
+
+        double ans = 1.0;
+        while (m > 0) {
+            if (m % 2 == 1) {
+                ans *= x;
+                m -= 1;
+            } else {
+                x *= x;
+                m /= 2;
+            }
+        }
         return ans;
-        
     }
 };
