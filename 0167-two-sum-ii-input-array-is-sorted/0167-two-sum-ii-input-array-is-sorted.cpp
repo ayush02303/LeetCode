384b5bd2483453ca146dd2c5f9870a13949b41cc
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-02 19:28:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (93.5%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,10 +6,10 @@ public:
 
         while( low<= high){
             int sum = numbers[low] + numbers[high]; 
-            if( sum == target) return { low+1, high+1}; 
-            else if( sum < target) low+=1;
+            if( sum == target) return { low+1 , high +1}; 
+            else if( sum < target) low+=1; 
             else high--; 
-        }
+        } 
         return {};
         
     }
