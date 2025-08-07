ef33949fbd8ab0a879364cc8bc68e3951368344d
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-07 09:05:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (19.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st; 
-
         for( auto ch : s){
-            if(st.empty()) st.push(ch); 
-            else if( st.top() == '(' && ch == ')' || st.top() == '{' && ch == '}' || st.top() == '[' && ch == ']' ) st.pop(); 
-            else{
-                st.push(ch); 
-            }
-
+            if( st.empty()) st.push(ch); 
+            else if( st.top() == '[' && ch == ']' || st.top() == '{' && ch == '}' || st.top() == '(' && ch == ')') st.pop(); 
+            else st.push(ch);
         }
-        return st.empty();
+        return st.empty(); 
     }
 };
