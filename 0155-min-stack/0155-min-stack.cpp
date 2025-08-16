6b1e801bf09e4f08445d062c510f5fc622f33ee6
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-16 11:09:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,24 @@
 class MinStack {
 public:
-    stack<pair<int,int>>st; 
+    stack<int>st; 
     MinStack() {
         
     }
     
     void push(int val) {
-        if(st.empty()) st.push({val,val}); 
-        else{
-            st.push({val, min(val,st.top().second )});
-        }
+        
         
     }
     
     void pop() {
-        st.pop(); 
         
     }
     
     int top() {
-        return st.top().first; 
         
     }
     
     int getMin() {
-        return st.top().second;
         
     }
 };
