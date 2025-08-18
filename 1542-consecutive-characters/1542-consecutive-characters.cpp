01a6a48c831d04579ff24fd1c6ac31c0fc668787
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-18 23:19:52 +0530

    Time: 0 ms (100%), Space: 9.5 MB (48.81%) - LeetHub

--- 1542-consecutive-characters/1542-consecutive-characters.cpp
+++ 1542-consecutive-characters/1542-consecutive-characters.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int n = s.size(); 
+        int maxi = 1; 
         int count = 1; 
-        int maxi = 1;
+        int n = s.size(); 
 
-        for( int i = 1 ; i< n ; i++){
-            if( s[i] == s[i-1] ){
+        for( int i = 1 ; i< n ;i++){
+            if( s[i] == s[i-1]){
                 count+=1; 
-
-                maxi = max(count, maxi); 
+                maxi = max(maxi,count); 
             }
-            else count =1 ;
+            else count =1 ; 
         }
-        return maxi; 
-
-
+        return maxi;
     }
 };
