08038f098fb91a7242ae2cc29a623ee8a038b5a2
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-21 10:19:01 +0530

    Time: 0 ms (100%), Space: 11.5 MB (83.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> lastIndex; // char → last index
+        vector<int> lastIndex(256, -1);  // stores last seen index of each char
         int left = 0, maxLen = 0;
 
         for (int right = 0; right < s.size(); right++) {
-            if (lastIndex.count(s[right])) {
+            if (lastIndex[s[right]] != -1) {
+                // move left pointer if char was seen inside window
                 left = max(left, lastIndex[s[right]] + 1);
             }
-            lastIndex[s[right]] = right;
+            lastIndex[s[right]] = right;  // update last seen index
             maxLen = max(maxLen, right - left + 1);
         }
+
         return maxLen;
     }
 };
