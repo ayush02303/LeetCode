6a5d629a680a47e79d152dee4d5f37ce2bb1e8b5
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-22 20:41:44 +0530

    Time: 35 ms (12.72%), Space: 36.7 MB (7.37%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> result;
-        int n = s.size(), m = p.size();
-        if (n < m) return result;
-
-        vector<int> pCount(26, 0), sCount(26, 0);
-
-        // count frequency of p
-        for (char c : p) {
-            pCount[c - 'a']++;
+     bool isValid( vector<int>&count){
+        return count == vector<int>(26,0); 
+    }
+    vector<int> findAnagrams(string txt, string pat ) {
+          vector<int>count(26,0); 
+        
+        for( int i = 0 ; i< pat.size() ; i++){
+            count[ pat[i] - 'a']++; 
         }
-
-        // first window of size m
-        for (int i = 0; i < m; i++) {
-            sCount[s[i] - 'a']++;
+        
+        int j = 0, i = 0; 
+        vector<int>result; 
+        
+        while( j< txt.size()){
+            count[ txt[j] - 'a']--;
+            
+            if( j-i+1 == pat.size()){
+                if( isValid(count)) result.push_back(i); 
+                
+                count[txt[i] - 'a']++; 
+                i++; 
+            } 
+            j++;
         }
-
-        if (sCount == pCount) result.push_back(0);
-
-        // slide the window
-        for (int i = m; i < n; i++) {
-            sCount[s[i] - 'a']++;              // include new char
-            sCount[s[i - m] - 'a']--;          // remove old char
-
-            if (sCount == pCount) {
-                result.push_back(i - m + 1);
-            }
-        }
-
         return result;
+        
     }
 };
