54123ad4579036ad4531e17804221ec99b00f4df
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-22 20:39:45 +0530

    Time: 2 ms (30.25%), Space: 14.8 MB (13.52%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int>ans; 
-        unordered_map<int,int>mp; 
-
-        for( auto num : nums){
-            mp[num]++; 
-        }
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for( auto num : nums) mp[num]++; 
         
         for( auto num : mp){
             if( num.second == 1) ans.push_back(num.first); 
         }
-        return ans;
+        return ans; 
     }
 };
