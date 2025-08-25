8b3bce17e8a1958791ec37d451a181f42242e474
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-25 11:36:07 +0530

    Time: 3 ms (14.19%), Space: 9.4 MB (18.48%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    bool isValid( char &ch){
-         if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'U') return true;
-        else return false;
-
-    }
     bool halvesAreAlike(string s) {
-        int  n = s.size(); 
+        int n = s.size(); 
         int i = 0; 
-        int j = n/2;
-        int lcount = 0; 
-        int rcount = 0;  
+        int j = n/2; 
+        int lcount = 0, rcount = 0; 
 
-        while( i< j && j < n){
-            if( isValid( s[i])) lcount++; 
-            if( isValid(s[j])) rcount++; 
+        unordered_set<char>st; 
+        st.insert('u');
+         st.insert('o');
+          st.insert('i');
+           st.insert('e');
+            st.insert('a');
+             st.insert('A');
+              st.insert('O');
+               st.insert('I');
+                st.insert('E');
+                 st.insert('U');
+
+        while( i < j && j< n){
+            if(st.find(s[i]) != st.end()) lcount++; 
+            if( st.find(s[j]) != st.end() ) rcount++; 
 
             i++; 
-            j++;  
- 
+            j++; 
         }
-        return lcount == rcount; 
-
-
+        return lcount == rcount;
         
     }
 };
