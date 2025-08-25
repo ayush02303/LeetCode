9b7efe8688de81ca9598b6b520a5a3a136d2dab5
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-25 11:13:49 +0530

    Time: 0 ms (100%), Space: 9.1 MB (76.09%) - LeetHub

---
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid( char &ch){
+        if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'U') return true;
+        else return false;
+    }
+    bool halvesAreAlike(string s) {
+        int lcount = 0, rcount = 0; 
+        int n = s.size(); 
+        int i = 0; 
+        int j = n/2; 
+        
+        while(  i< j && j< n){
+            if( isValid( s[i])) lcount++; 
+            if( isValid( s[j])) rcount++; 
+            i++; 
+            j++;
+        }
+        return lcount == rcount; 
+        
+        
+    }
+};
