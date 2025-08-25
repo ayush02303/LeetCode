09c745cb2bcf2a118046a92d22bc842646138557
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-25 11:20:13 +0530

    Time: 0 ms (100%), Space: 9.2 MB (52.19%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool isValid( char &ch){
-        if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'U') return true;
+         if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'U') return true;
         else return false;
+
     }
     bool halvesAreAlike(string s) {
-        int lcount = 0, rcount = 0; 
-        int n = s.size(); 
+        int  n = s.size(); 
         int i = 0; 
-        int j = n/2; 
-        
-        while(  i< j && j< n){
+        int j = n/2;
+        int lcount = 0; 
+        int rcount = 0;  
+
+        while( i< j && j < n){
             if( isValid( s[i])) lcount++; 
-            if( isValid( s[j])) rcount++; 
+            if( isValid(s[j])) rcount++; 
+
             i++; 
-            j++;
+            j++;  
+ 
         }
         return lcount == rcount; 
-        
+
+
         
     }
 };
