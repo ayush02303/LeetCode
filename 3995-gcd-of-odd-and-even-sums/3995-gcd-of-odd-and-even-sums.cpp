71e83293513b98fa27c3d3ee94e3b984625ae6af
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-27 12:59:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (49.7%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        // From math deduction:
-        // sumOdd = n^2
-        // sumEven = n*(n+1)
-        // gcd(sumOdd, sumEven) = n
         return n;
+        
     }
 };
