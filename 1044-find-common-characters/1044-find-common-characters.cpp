c1e31765ca8edabb4addf3fcdac6fd6bff6edcfc
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-08-30 10:48:38 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.72%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    void fillCountArray(string &word, int count[26]) {
-        for(char &ch : word) {
-            count[ch-'a']++;
+    void isValid( string word , int count[26]){
+        for( auto ch : word){
+            count[ ch - 'a']++; 
         }
-    }
-    vector<string> commonChars(vector<string>& words) {
-        vector<string> result;
-        
-        int n = words.size();
-        
+
+    } 
+    vector<string> commonChars(vector<string>& word) {
+        vector<string>result; 
+
         int count[26] = {0};
-        
-        fillCountArray(words[0], count);
+        isValid( word[0], count); 
 
-        for(int i = 1; i<n; i++) {
-            int temp[26] = {0};
-            
-            fillCountArray(words[i], temp);
+        for( int i = 1 ; i < word.size() ; i++){
+                int temp[26] = {0}; 
+                isValid( word[i], temp); 
 
-            for(int i = 0; i<26; i++) {
-                if(count[i] != temp[i])
-                    count[i] = min(count[i], temp[i]);
-            }
-            
+                for( int i = 0; i< 26 ; i++){
+                    if( count[i] != temp[i]) count[i] = min( count[i], temp[i]); 
+                }
         }
-        
-        for(int i = 0; i<26; i++) {
-            if(count[i] != 0) {
-                int c = count[i];
-                while(c--) {
-                    result.push_back(string(1, i+'a'));
+
+        for( int i = 0; i < 26 ; i++){
+            if( count[i] != 0){
+                while( count[i]--){
+                    result.push_back( string(1, i+ 'a'));
                 }
             }
         }
-        
         return result;
     }
 };
