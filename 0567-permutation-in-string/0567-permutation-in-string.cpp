8bafdb6577a9a73d1f2cfc7c0161ab6d3f6fa2f6
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-01 12:04:22 +0530

    Time: 1046 ms (5.67%), Space: 60.6 MB (7.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.length();
-        int m = s2.length();
+        int m = s1.size(); 
+        int n = s2.size(); 
 
-        // If s1 is longer than s2, it cannot be a substring
-        if (n > m) return false;
+        if( m > n) return false; 
 
-        // Sort the string s1
-        string sorted_s1 = s1;
-        sort(sorted_s1.begin(), sorted_s1.end());
+        sort( s1.begin(), s1.end()); 
 
-        // Iterate over each substring of s2 of length equal to s1
-        for (int i = 0; i <= m - n; i++) {
-            // Extract a substring of length n from s2 starting at index i
-            string temp = s2.substr(i, n);
+        for( int i = 0; i<= n- m; i++){
+            string substring = s2.substr(i,m);
+            sort( substring.begin(), substring.end()); 
 
-            // Sort the substring
-            sort(temp.begin(), temp.end());
-
-            // If the sorted substring matches the sorted s1, return true
-            if (temp == sorted_s1) return true;
+            if( s1 == substring) return true; 
         }
-
-        // No permutation found in s2
         return false;
+
+
+        
     }
 };
