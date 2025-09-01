903f3f60e0a0990ee036c7d18f784e6330b9dd48
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-01 12:53:52 +0530

    Time: 49 ms (16.2%), Space: 53.7 MB (26.21%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -1,29 +1,23 @@
 /**
- * @param {any} val
+ * @param {string} val
  * @return {Object}
  */
 var expect = function(val) {
-    return {
-        toBe: function(otherVal) {
-            if (val === otherVal) {
-                return true;
-            } else {
-                throw new Error("Not Equal");
-            }
-        },
-        notToBe: function(otherVal) {
-            if (val !== otherVal) {
-                return true;
-            } else {
-                throw new Error("Equal");
-            }
+    return{
+        toBe : function( otherVal){
+            if( val === otherVal) return true; 
+            else throw new Error("Not Equal")
+        }, 
+        notToBe : function( otherVal){
+            if( val !== otherVal) return true; 
+            else throw new Error("Equal")
+
         }
     }
+    
 };
 
 /**
- * Example usage:
- * expect(5).toBe(5);       // ✅ returns true
- * expect(5).notToBe(10);   // ✅ returns true
- * expect(5).notToBe(5);    // ❌ throws "Equal"
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
  */
