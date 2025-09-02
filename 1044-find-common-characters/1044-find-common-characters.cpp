5add26dcea67a2f7f65f00e0844e532dbd0469a3
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-02 12:56:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.55%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -5,26 +5,26 @@ public:
             count[ ch - 'a']++; 
         }
 
-    } 
-    vector<string> commonChars(vector<string>& word) {
-        vector<string>result; 
+    }
+    vector<string> commonChars(vector<string>& words) {
+        int count[26] = {0}; 
+        isValid( words[0], count);
 
-        int count[26] = {0};
-        isValid( word[0], count); 
+        for( int i =1 ; i < words.size() ; i++){
+            int temp[26] = {0};
+            isValid( words[i], temp);
 
-        for( int i = 1 ; i < word.size() ; i++){
-                int temp[26] = {0}; 
-                isValid( word[i], temp); 
-
-                for( int i = 0; i< 26 ; i++){
-                    if( count[i] != temp[i]) count[i] = min( count[i], temp[i]); 
-                }
+            for( int i = 0; i < 26 ; i++){
+                if( count[i] != temp[i]) count[i] = min( count[i], temp[i]);
+            }
         }
 
-        for( int i = 0; i < 26 ; i++){
-            if( count[i] != 0){
+        vector<string>result; 
+
+        for( int i = 0; i< 26 ; i++){
+            if( count[i]!= 0){
                 while( count[i]--){
-                    result.push_back( string(1, i+ 'a'));
+                    result.push_back( string( 1, i + 'a'));
                 }
             }
         }
