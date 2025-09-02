e80eb15480f8026f8a42b9e6771028f5e9f548c1
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-02 11:25:03 +0530

    Time: 46 ms (31.01%), Space: 53.3 MB (62.57%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -3,17 +3,17 @@
  * @return {Object}
  */
 var expect = function(val) {
-    return{
-        toBe : function(val1){
-            if( val1 === val) return true
-            else throw new Error("Not Equal")
-        }, 
-        notToBe : function( val2){
-            if( val !== val2) return true
-            else throw new Error("Equal")
-
-        }
+    return {
+         toBe : function( val1){
+        if( val1 === val ) return true
+        else throw new Error("Not Equal")
+    }, 
+     notToBe : function( val2){
+        if( val2 !== val ) return true
+        else throw new Error("Equal")
     }
+    }
+   
     
 };
 
