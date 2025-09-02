4622f1ebda4c6d5176a7e2920d48ff97fe30c4e4
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-02 12:12:07 +0530

    Time: 1058 ms (5.02%), Space: 60.5 MB (7.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.size(); // small string 
-        int n2 = s2.size();  // parent string 
+        int n1 = s1.size(); 
+        int n2 = s2.size(); 
 
-        if( n1 > n2) return false; 
-        sort( s1.begin(), s1.end()); 
+        sort(s1.begin(),s1.end()); 
 
-        for( int i = 0 ; i<=  n2-n1 ; i++){
-            string s3 = s2.substr(i,n1); 
-            sort( s3.begin(), s3.end());
+        for( int i = 0 ;i <= n2-n1 ; i++){
+            string s3 = s2.substr(i, n1); 
+            sort( s3.begin(), s3.end()); 
 
-            if( s3 == s1) return true;
-
+            if( s1 == s3) return true; 
 
         }
         return false;
-        
     }
 };
