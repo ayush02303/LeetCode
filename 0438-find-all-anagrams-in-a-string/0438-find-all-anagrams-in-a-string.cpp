c4288b8c5f762854a573be635654a2dc475f5777
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-09-13 12:42:34 +0530

    Time: 39 ms (11.67%), Space: 36.8 MB (7.64%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    bool isValid( vector<int> & count){
+     bool isValid( vector<int> &count){
         return count == vector<int>(26,0); 
     }
-    vector<int> findAnagrams(string s, string p) {
-        vector<int>ans; 
-        vector<int>count( 26, 0); 
-
-        for( int i = 0 ; i< p.size() ; i++){
-            count[ p[i] - 'a']++; 
+    
+    vector<int> findAnagrams(string txt, string pat ) {
+          // code here
+        vector<int>count(26,0); 
+        
+        for( int i = 0; i < pat.size() ; i++){
+            count[ pat[i] - 'a' ]++; 
         }
-
-        int i = 0 ,j =0; 
-
-        while( j < s.size()){
-            count[ s[j] - 'a']--; 
-
-            if( j-i+1 == p.size()){
-                if( isValid( count)) ans.push_back(i); 
-
-                count[ s[i] - 'a']++; 
-                i++; 
+        
+        int i = 0 , j = 0 ; 
+        vector<int>ans; 
+        
+        while( j < txt.size()){
+            count[ txt[j] - 'a']--; 
+            
+            if( j-i+1 == pat.size()){
+                if( isValid( count) ) ans.push_back(i);
+                
+                 count[ txt[i] - 'a']++; 
+            i++; 
+                
             }
             j++; 
-
+           
         }
-        return ans; 
+        return ans;
+        
     }
 };
