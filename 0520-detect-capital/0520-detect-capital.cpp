1db4b62937ab3ef5cfc7f29dda445ef8c27e7783
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-10-01 09:55:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.25%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -4,11 +4,13 @@ public:
         int uppercount = 0; 
         for( auto ch : word){
             if( isupper(ch)) uppercount+=1; 
+          
         }
-        if( uppercount == word.size()) return true; 
-        else if( uppercount == 0 ) return true; 
-        else if( uppercount == 1 && isupper(word[0]) ) return true;
-        else return false; 
-         
+          if( uppercount == 0) return true; 
+            else if( uppercount == word.size() )return true; 
+            else if( uppercount == 1 &&  isupper(word[0])) return true; 
+            else return false;
+
+        
     }
 };
