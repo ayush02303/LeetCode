1439b5c6ff77caccdf0d0deb133e33eb2f75a2e2
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-10-11 20:13:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (52.3%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int uppercount = 0; 
+        int uppercount = 0 ; 
         for( auto ch : word){
             if( isupper(ch)) uppercount+=1; 
-          
         }
-          if( uppercount == 0) return true; 
-            else if( uppercount == word.size() )return true; 
-            else if( uppercount == 1 &&  isupper(word[0])) return true; 
-            else return false;
-
+        if( uppercount == word.size()) return true; 
+        else if( uppercount == 0) return true; 
+        else if( uppercount == 1 && isupper(word[0])) return true; 
+        else return false;
         
     }
 };
