5130a7ce90c7db4c0354fcbacefaea7205caf523
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-10-17 00:02:43 +0530

    Time: 0 ms (100%), Space: 31.4 MB (9.12%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -7,9 +7,9 @@ public:
             int mid = low + (high - low)/2; 
             if( nums[mid] == target) return mid; 
             else if( nums[mid] < target) low = mid +1 ; 
-            else high = mid -1 ;
+            else high = mid -1 ; 
         }
-        return -1 ;
+        return -1; 
         
     }
 };
