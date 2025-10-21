bff8a91240632c2bb27fe419453014a45fbb2440
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-10-21 11:25:36 +0530

    Time: 0 ms (100%), Space: 9.9 MB (62.57%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -17,7 +17,7 @@ public:
         while( fast!= NULL && fast->next!= NULL){
             slow = slow->next; 
             fast = fast->next->next; 
-        }
+        } 
         return slow;
         
     }
