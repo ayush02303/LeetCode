b0d4637b4afcc329bd2980a44bc67eff3cc40f66
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-11-13 10:52:15 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.25%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -1,24 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head; 
-        ListNode* fast = head; 
+        int count = 0;
+        ListNode* temp = head;
 
-        while( fast!= NULL && fast->next!= NULL){
-            slow = slow->next; 
-            fast = fast->next->next; 
-        } 
-        return slow;
-        
+        // Step 1: Count total nodes
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+
+        // Step 2: Find middle index
+        int mid = count / 2;  // If even, returns the second middle as per LeetCode definition
+
+        // Step 3: Traverse again to the middle
+        temp = head;
+        for (int i = 0; i < mid; i++) {
+            temp = temp->next;
+        }
+
+        return temp;
     }
 };
