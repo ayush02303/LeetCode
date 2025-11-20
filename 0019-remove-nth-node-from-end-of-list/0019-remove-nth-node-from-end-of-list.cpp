9a446cc84ec8040544b2932f2607cb59111206fc
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-11-20 19:30:26 +0530

    Time: 0 ms (100%), Space: 15 MB (25.06%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,35 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int k ) {
-         int count = 0;
-        ListNode* temp = head;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int count = 0; 
+        ListNode* temp = head; 
 
-        // First pass: Count total number of nodes
-        while (temp != nullptr) {
-            count++;
-            temp = temp->next;
+        while(temp != NULL){
+            count+=1; 
+            temp = temp->next; 
+        }
+        if( count ==  n ){
+            ListNode* temp1 = head->next; 
+            delete head; 
+            return temp1; 
         }
 
-        // Edge case: removing the head node
-        if (k == count) {
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
+        int length = count- n ; 
+        temp = head; 
+
+        for( int i = 1 ; i< length ; i++){
+            temp = temp->next; 
         }
+        ListNode* deleteNode  = temp->next; 
+        temp->next = temp->next->next; 
+        delete deleteNode; 
+        return head; 
 
-        // Find (count - k - 1)-th node: node before the one to remove
-        int target = count - k;
-        temp = head;
-        for (int i = 1; i < target; ++i) {
-            temp = temp->next;
-        }
 
-        // Remove the k-th node from end
-        ListNode* nodeToDelete = temp->next;
-        temp->next = temp->next->next;
-        delete nodeToDelete;
-
-        return head;
         
     }
 };
