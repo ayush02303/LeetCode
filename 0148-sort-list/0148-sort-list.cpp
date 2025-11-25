3768425ff94ecd12dd3534614d6249254a0e4860
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2025-11-25 11:05:51 +0530

    Time: 14 ms (68.25%), Space: 58.2 MB (54.29%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,21 +11,21 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head; 
         vector<int>ans; 
-
+        ListNode* temp = head; 
         while(temp!= NULL){
             ans.push_back(temp->val); 
             temp = temp->next; 
         }
-        sort(ans.begin(),ans.end()); 
+        sort( ans.begin() , ans.end()); 
         temp = head; 
         int i = 0; 
-        while(temp!= NULL){
-            temp->val = ans[i++]; 
+        while( temp!= NULL){
+            temp->val = ans[i]; 
+            i++; 
             temp = temp->next; 
         }
-        return head; 
+        return head;
         
     }
 };
