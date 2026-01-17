3e02c02d687eee993e0afacbda05ae04bf010918
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-17 13:06:05 +0530

    Time: 84 ms (56.71%), Space: 93.1 MB (49.39%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int halveArray(vector<int>& nums) {
-        priority_queue<double >pq; 
-        double sum = 0 ; 
+        priority_queue<double>pq; 
+        double sum = 0; 
+
         for( auto x : nums){
             sum+=x; 
             pq.push(x); 
         }
-        double  target = sum/2.0; 
-        int operations = 0; 
+        int operations = 0 ; 
+        double  target  = sum/2.0;
+
 
         while( sum > target ){
-            double ele = pq.top(); 
-            double  half = ele/2.0;
-            pq.pop(); 
-            sum-= half; 
+            double  top = pq.top(); 
+            pq.pop();
+            double half = top/2.0 ; 
 
+            sum-= half; 
             pq.push(half); 
-            operations++ ;  
+            operations++;  
 
         }
         return operations;
-
-
         
     }
 };
