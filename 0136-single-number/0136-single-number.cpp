0f7213300902d6cebbf577b9416fe20be21f2d50
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-22 13:12:55 +0530

    Time: 11 ms (14.32%), Space: 24 MB (5.4%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,13 +3,13 @@ public:
     int singleNumber(vector<int>& nums) {
         unordered_map<int,int>mp; 
 
-        for( auto num : nums){
-            mp[num]++; 
+        for( auto x : nums){
+            mp[x]++; 
         }
-
-        for( auto num : mp){
-            if(num.second == 1) return num.first; 
+        for( auto x : nums){
+            if( mp[x] == 1) return x; 
         }
-        return -1;
+        return -1; 
+        
     }
 };
