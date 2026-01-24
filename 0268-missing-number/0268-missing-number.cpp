c54eb9bc07c588316d228747da1f570a749f8f19
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-24 12:44:11 +0530

    Time: 19 ms (8.32%), Space: 27.9 MB (5.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& arr) {
-         // code here
-        int n= arr.size(); 
-        int sum= (n*(n+1))/2; 
-        int sum2=0; 
-        
-        for(int i=0; i<n;i++)
-        {
-            sum2+=arr[i]; 
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st;
+
+        // insert all elements
+        for (int x : nums) {
+            st.insert(x);
         }
-        
-        int miss= sum-sum2; 
-        return miss;
-        
+
+        // check from 0 to n
+        for (int i = 0; i <= n; i++) {
+            if (st.find(i) == st.end()) {
+                return i;
+            }
+        }
+
+        return -1; // safety
     }
 };
