ece88023f2e235025e5a8d8ce1cad9eea1219277
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-24 12:53:22 +0530

    Time: 15 ms (10.5%), Space: 27.9 MB (5.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> st;
+        unordered_set<int>st; 
 
-        // insert all elements
-        for (int x : nums) {
-            st.insert(x);
-        }
+        for( auto x : nums) st.insert(x); 
 
-        // check from 0 to n
-        for (int i = 0; i <= n; i++) {
-            if (st.find(i) == st.end()) {
-                return i;
-            }
-        }
+        for( int i = 0 ; i <= nums.size() ; i++){
+            if( st.find(i)  == st.end()) return i ; 
 
-        return -1; // safety
+        } 
+        return -1; 
+        
     }
 };
