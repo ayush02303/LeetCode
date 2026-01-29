be88a489c0e54f124e654c58e433a9bfbb11cf8a
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-29 23:31:52 +0530

    Time: 1 ms (35.51%), Space: 28.1 MB (62.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        for( int i = 0 ; i< nums.size() ; i++){
-            int ele = nums[i]; 
-            int count = 0; 
-
-            for( int j = 0; j < nums.size() ; j++){
-                if( nums[j] == ele) count++; 
-
-            }
-            if( count > nums.size()/2) return nums[i]; 
-        }
-       return -1;  
+        sort(nums.begin(), nums.end()); 
+        return nums[nums.size()/2]; 
+        
     }
 };
