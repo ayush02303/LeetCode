2b41db0da744656498b23a61daa51c9138bc39db
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-29 14:48:22 +0530

    Time: 0 ms (100%), Space: 15.7 MB (66.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size(); 
-        int pivot=-1; 
+        int pivot = -1; 
+        int n = nums.size(); 
 
-        for(int i=n-2;i>=0;i--)
-        {
-            if(nums[i] <nums[i+1])
-            {
-                pivot=i; 
-                break;
-            }
-
-        }
-        if(pivot==-1)
-        {
-            reverse(nums.begin(),nums.end()); 
-            return; 
-        }
-
-        for(int j=n-1;j>pivot;j--)
-        {
-            if(nums[j] > nums[pivot])
-            {
-                swap(nums[j],nums[pivot]); 
+        for( int i = n-2; i>= 0 ;i--){
+            if( nums[i] < nums[i+1]){
+                pivot = i; 
                 break; 
-
             }
+        
+        
+        }
+        if( pivot == -1){
+            reverse(nums.begin() , nums.end()); 
+            return ;
         }
 
-        int i=pivot+1,j=n-1;
-        while(i<=j)
-        {
-            swap(nums[i++],nums[j--]);
+        for( int i = n-1; i> pivot; i--){
+            if( nums[i] > nums[pivot]){
+                swap(nums[pivot], nums[i]); 
+                break; 
+            }
+
+
         }
-    
+        int i = pivot+1; int j = nums.size()-1; 
+        while( i< j){
+            swap( nums[i] , nums[j]); 
+            i++;j--;
+        }
+        
     }
 };
