5b2fd84e75907bcf3784621f63eb4150d1a01b58
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-30 15:00:34 +0530

    Time: 0 ms (100%), Space: 9.2 MB (97.46%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-
-        if( matrix.size() == 0) return {}  ; 
+        if( matrix.empty()) return {} ; 
         int m = matrix.size(); 
         int n = matrix[0].size(); 
 
-        vector<int>result;
-
-        int top = 0; 
-        int bottom = m-1; 
-        int left = 0 ; 
+        int top = 0 ; 
+        int bottom = m-1;
+        int left = 0; 
         int right = n-1; 
 
-        int id = 0 ;
+        int id = 0; 
 
+        vector<int>ans; 
 
         while( top <= bottom && left <= right){
 
-
             if( id == 0){
-                for( int i = left; i <= right ; i++ ){
-                    result.push_back(matrix[top][i]); 
-                }
-                  top++; 
-            }
+                for( int i = left ; i<= right ; i++){
+                    ans.push_back(matrix[top][i]);
 
-            if( id == 1){
-                for( int i = top ; i<= bottom; i++){
-                    result.push_back(matrix[i][right]); 
                 }
-                right--; 
+                top++;
+            }
+            if(id == 1){
+                for( int i = top ; i <= bottom ; i++){
+                    ans.push_back(matrix[i][right]); 
+                }
+                right--;
             }
             if( id == 2){
                 for( int i = right ; i >= left ; i--){
-                    result.push_back(matrix[bottom][i]); 
+                    ans.push_back(matrix[bottom][i]); 
                 }
-                bottom--; 
+                bottom--;
+
             }
             if( id == 3){
                 for( int i = bottom ; i>= top ; i--){
-                    result.push_back(matrix[i][left]); 
+                    ans.push_back(matrix[i][left]); 
                 }
                 left++; 
-            }
-            id+=1; 
-            if( id == 4) id = 0;
 
-          
+            }
+
+
+
+
+            id+=1; 
+            if( id == 4) id = 0; 
+
 
         }
-        return result; 
+        return ans; 
+        
     }
 };
