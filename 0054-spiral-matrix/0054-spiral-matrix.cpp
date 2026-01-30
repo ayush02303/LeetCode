fe8f6e3d04e4f568f39727b5060836b749bccf95
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-01-30 14:00:08 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,59 +1,55 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if(matrix.size() == 0)
-            return {};
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> result;
-        int top   = 0;
-        int down  = m-1;
-        int left  = 0;
-        int right = n-1;
-        
-        int id = 0;
-        //id
-        //0   -> left  to right
-        //1   -> top   to down
-        //2   -> right to left
-        //3   -> down  to top
 
-        while(top <= down && left <= right) {
-            //left to right
-            if(id == 0) {
-                for(int i = left; i<=right; i++) {
-                    result.push_back(matrix[top][i]);
+        if( matrix.size() == 0) return {}  ; 
+        int m = matrix.size(); 
+        int n = matrix[0].size(); 
+
+        vector<int>result;
+
+        int top = 0; 
+        int bottom = m-1; 
+        int left = 0 ; 
+        int right = n-1; 
+
+        int id = 0 ;
+
+
+        while( top <= bottom && left <= right){
+
+
+            if( id == 0){
+                for( int i = left; i <= right ; i++ ){
+                    result.push_back(matrix[top][i]); 
                 }
-                top++;
+                  top++; 
             }
-            
-            //top to down
-            if(id == 1) {
-                for(int i = top; i<=down; i++) {
-                    result.push_back(matrix[i][right]);
+
+            if( id == 1){
+                for( int i = top ; i<= bottom; i++){
+                    result.push_back(matrix[i][right]); 
                 }
-                right--;
+                right--; 
             }
-            
-            //right to left
-            if(id == 2) {
-                for(int i = right; i>=left; i--) {
-                    result.push_back(matrix[down][i]);
+            if( id == 2){
+                for( int i = right ; i >= left ; i--){
+                    result.push_back(matrix[bottom][i]); 
                 }
-                down--;
+                bottom--; 
             }
-            
-            //down to top
-            if(id == 3) {
-                for(int i = down; i>=top; i--) {
-                    result.push_back(matrix[i][left]);
+            if( id == 3){
+                for( int i = bottom ; i>= top ; i--){
+                    result.push_back(matrix[i][left]); 
                 }
-                left++;
+                left++; 
             }
-            
-            id++; 
-            if( id == 4 ) id = 0;  
+            id+=1; 
+            if( id == 4) id = 0;
+
+          
+
         }
-        return result;
+        return result; 
     }
 };
