9594050bc9b37af12af3d34380f2b035fd5d546c
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-02-02 10:12:32 +0530

    Time: 0 ms (100%), Space: 15.1 MB (16.2%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int sum = 0;
+        int sum = 0; 
 
-        for (int i = 0; i < n; i++) {
-            sum += mat[i][i];                 // primary diagonal
-            sum += mat[i][n - 1 - i];         // secondary diagonal
+        for( int i = 0; i< mat.size() ; i++){
+            sum+= mat[i][i]; 
+            sum+=mat[i][mat.size()-1-i]; 
         }
 
-        // If n is odd, subtract the middle element once
-        if (n % 2 == 1) {
-            sum -= mat[n / 2][n / 2];
+        if(mat.size()%2 == 1){
+            sum-= mat[mat.size()/2][mat.size()/2]; 
         }
-
-        return sum;
+        return sum; 
+        
     }
 };
