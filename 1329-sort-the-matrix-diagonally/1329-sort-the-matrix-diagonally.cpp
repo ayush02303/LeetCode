562def5cc35d5884e0757dc0b0a86410ff61bc8b
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-02-02 11:12:31 +0530

    Time: 8 ms (6.19%), Space: 13.7 MB (59.4%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        unordered_map<int, vector<int>> mp;
-        int m = mat.size();
-        int n = mat[0].size();
-
-        // Step 1: collect diagonals
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mp[i - j].push_back(mat[i][j]);
+        unordered_map<int,vector<int>>mp; 
+        
+        for( int i = 0; i< mat.size() ; i++){
+            for( int j = 0; j< mat[0].size() ; j++){
+                mp[i-j].push_back(mat[i][j]); 
             }
         }
 
-        // Step 2: sort each diagonal
-        for (auto &x : mp) {
-            sort(x.second.begin(), x.second.end());
+        for( auto &it : mp){
+            sort(it.second.begin() , it.second.end()); 
         }
 
-        // Step 3: put back values (bottom-right → top-left)
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                mat[i][j] = mp[i - j].back();
-                mp[i - j].pop_back();
+
+        for( int i = mat.size()-1. ; i>= 0 ; i--){
+            for( int j = mat[0].size()-1 ; j>= 0 ; j--){
+                mat[i][j] = mp[i-j].back(); 
+                mp[i-j].pop_back(); 
             }
-        }
 
-        return mat;
+        }
+        return mat; 
     }
 };
