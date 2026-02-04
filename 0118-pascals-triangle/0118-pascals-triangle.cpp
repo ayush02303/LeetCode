56e0e40495fc0cfba25a27f7d50eb5702a8895a0
Ayush Badola <142813464+ayush02303@users.noreply.github.com>
2026-02-04 10:36:33 +0530

    Time: 2 ms (15.4%), Space: 9.7 MB (63.4%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -6,10 +6,11 @@ public:
         for( int i = 0 ; i< numRows ; i++){
             result[i] = vector<int>(i+1, 1); 
 
-            for( int j = 1 ; j< i ; j++){
+            for( int j = 1 ; j< i ; j++ ){
                 result[i][j] = result[i-1][j] + result[i-1][j-1]; 
             }
-        }
+
+        }        
         return result; 
     }
 };
